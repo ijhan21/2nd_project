4eac65e1a0e413211e348b7e4da4f1c11ef68cf8
Harry Baek <01697550772@naver.com>
2021-11-11 15:31:42 +0900

    오후3시31분

--- qrkiosk/board/views.py
+++ qrkiosk/board/views.py
@@ -7,11 +7,11 @@ from .models import Board
 
 def index(request):
     all_boards = Board.objects.all().order_by("-pub_date") # 모든 데이터 조회, 내림차순(-표시) 조회
-    paginator = Paginator(all_boards, 5)
+    paginator = Paginator(all_boards, 10)
     page = int(request.GET.get('page', 1))
     board_list = paginator.get_page(page)
 
-    return render(request, 'board/index.html', {'title':'Board List', 'board_list':board_list})
+    return render(request, 'board/index.html', {'title':'고객 게시판', 'board_list':board_list})
 
 def detail(request, board_id):
     board = Board.objects.get(id=board_id)
